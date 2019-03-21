0b92be36c804d67a329937a925569f7e695c78c2
Preethi Ramkumar <preethi.ramkumar@myob.com>
2019-03-21 11:50:36 +1100

    Initial commit

---
+++ src/main/java/com/myob/bowling/Game.java
@@ -0,0 +1,4 @@
+package main.java.com.myob.bowling;
+
+public class Game {
+}
---
+++ src/test/java/com/myob/bowling/GameTest.java
@@ -0,0 +1,12 @@
+package test.java.com.myob.bowling;
+
+import org.junit.Test;
+
+public class GameTest {
+
+  @Test
+  public void whenXthenY() {
+
+  }
+
+}
